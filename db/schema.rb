57f49abe177c9195b8b0e2b3201cbf49d5280468
handa1220 <108671890+handa1220@users.noreply.github.com>
2022-08-15 09:51:46 +0000

    [rest]4-9

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_14_083724) do
+ActiveRecord::Schema.define(version: 2022_08_15_094922) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -43,7 +43,6 @@ ActiveRecord::Schema.define(version: 2022_08_14_083724) do
   create_table "post_images", force: :cascade do |t|
     t.string "shop_name"
     t.text "caption"
-    t.integer "user_"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
